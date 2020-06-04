4f0f6295c80d1fb0d696a39ffa11c9b56763d61a
Arvin Singla <arvin.singla@gmail.com>
2020-06-03 22:56:52 -0400

    Refactor to remove image processing and greatly simply my life.

--- index.js
+++ index.js
@@ -1,134 +1,58 @@
-const express = require('express');
-const sharp = require('sharp');
-const morgan = require('morgan');
-const multer = require('multer');
-const Redis = require('ioredis');
-const request = require('request-promise-native');
-const sha1 = require('sha1');
-const Slack = require('slack-node');
-const upload = multer({ storage: multer.memoryStorage() });
-
-const SEVEN_DAYS = 7 * 24 * 60 * 60; // in seconds
-
-//
-// setup
-
+const express = require("express");
+const morgan = require("morgan");
+const Slack = require("slack-node");
+const bodyParser = require("body-parser");
+/**
+ * Setup
+ */
 const channel = process.env.SLACK_CHANNEL;
-const appURL = process.env.APP_URL;
-const redis = new Redis(process.env.REDIS_URL);
 
-//
-// slack
-
+/**
+ * Slack
+ */
 const slack = new Slack();
 slack.setWebhook(process.env.SLACK_URL);
 
-//
-// express
-
+/**
+ * Express
+ */
 const app = express();
-const port = process.env.PORT || 11000;
+const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
 app.listen(port, () => {
   console.log(`Express app running at http://localhost:${port}`);
 });
 
-//
-// routes
+/**
+ * Express Routes
+ */
+app.post("/", async (req, res, next) => {
+  const { payload } = req.body;
 
-app.post('/', upload.single('thumb'), async (req, res, next) => {
-  const payload = JSON.parse(req.body.payload);
+  const allowedPlexWebhooks = {
+    "media.play": "Started",
+    "media.scrobble": "Finished",
+    "media.rate": "Rated",
+    "library.new": "Added",
+  };
 
-  const isVideo = (['movie', 'episode'].includes(payload.Metadata.type));
-  const isAudio = (payload.Metadata.type === 'track');
-  const key = sha1(payload.Server.uuid + payload.Metadata.ratingKey);
-
-  // missing required properties
-  if (!payload.user || !payload.Metadata || !(isAudio || isVideo)) {
-    return res.sendStatus(400);
-  }
-
-  // retrieve cached image
-  let image = await redis.getBuffer(key);
-
-  // save new image
-  if (payload.event === 'media.play' || payload.event === 'media.rate') {
-    if (image) {
-      console.log('[REDIS]', `Using cached image ${key}`);
-    } else {
-      let buffer;
-      if (req.file && req.file.buffer) {
-        buffer = req.file.buffer;
-      } else if (payload.thumb) {
-        console.log('[REDIS]', `Retrieving image from  ${payload.thumb}`);
-        buffer = await request.get({
-          uri: payload.thumb,
-          encoding: null
-        });
-      }
-      if (buffer) {
-        image = await sharp(buffer)
-          .resize({
-            height: 75,
-            width: 75,
-            fit: 'contain',
-            background: 'white'
-          })
-          .toBuffer();
-
-        console.log('[REDIS]', `Saving new image ${key}`);
-        redis.set(key, image, 'EX', SEVEN_DAYS);
-      }
-    }
-  }
-
-  // post to slack
-  if ((payload.event === 'media.scrobble' && isVideo) || payload.event === 'media.rate') {
-    const location = await getLocation(payload.Player.publicAddress);
-
-    let action;
-
-    if (payload.event === 'media.scrobble') {
-      action = 'played';
-    } else if (payload.rating > 0) {
-      action = 'rated ';
-      for (var i = 0; i < payload.rating / 2; i++) {
-        action += ':star:';
-      }
-    } else {
-      action = 'unrated';
-    }
-
-    if (image) {
-      console.log('[SLACK]', `Sending ${key} with image`);
-      notifySlack(appURL + '/images/' + key, payload, location, action);
-    } else {
-      console.log('[SLACK]', `Sending ${key} without image`);
-      notifySlack(null, payload, location, action);
-    }
+  if (Object.keys(allowedPlexWebhooks).includes(payload.event)) {
+    notifySlack(payload, allowedPlexWebhooks[payload.event]);
+  } else {
+    console.log(`${payload.event} is not an allowed webhook`);
   }
 
   res.sendStatus(200);
-
 });
 
-app.get('/images/:key', async (req, res, next) => {
-  const exists = await redis.exists(req.params.key);
-
-  if (!exists) {
-    return next();
-  }
-
-  const image = await redis.getBuffer(req.params.key);
-  sharp(image).jpeg().pipe(res);
-});
-
-//
-// error handlers
+/**
+ * Error Handlers
+ */
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -138,12 +62,9 @@ app.use((err, req, res, next) => {
   res.send(err.message);
 });
 
-//
-// helpers
-
-function getLocation(ip) {
-  return request.get(`http://api.ipstack.com/${ip}?access_key=${process.env.IPSTACK_KEY}`, { json: true });
-}
+/**
+ * Helper Methods
+ */
 
 function formatTitle(metadata) {
   if (metadata.grandparentTitle) {
@@ -158,10 +79,10 @@ function formatTitle(metadata) {
 }
 
 function formatSubtitle(metadata) {
-  let ret = '';
+  let ret = "";
 
   if (metadata.grandparentTitle) {
-    if (metadata.type === 'track') {
+    if (metadata.type === "track") {
       ret = metadata.parentTitle;
     } else if (metadata.index && metadata.parentIndex) {
       ret = `S${metadata.parentIndex} E${metadata.index}`;
@@ -170,35 +91,32 @@ function formatSubtitle(metadata) {
     }
 
     if (metadata.title) {
-      ret += ' - ' + metadata.title;
+      ret += " - " + metadata.title;
     }
-  } else if (metadata.type === 'movie') {
+  } else if (metadata.type === "movie") {
     ret = metadata.tagline;
   }
 
   return ret;
 }
 
-function notifySlack(imageUrl, payload, location, action) {
-  let locationText = '';
-
-  if (location) {
-    const state = location.country_code === 'US' ? location.region_name : location.country_name;
-    locationText = `near ${location.city}, ${state}`;
-  }
-
-  slack.webhook({
-    channel,
-    username: 'Plex',
-    icon_emoji: ':plex:',
-    attachments: [{
-      fallback: 'Required plain-text summary of the attachment.',
-      color: '#a67a2d',
-      title: formatTitle(payload.Metadata),
-      text: formatSubtitle(payload.Metadata),
-      thumb_url: imageUrl,
-      footer: `${action} by ${payload.Account.title} on ${payload.Player.title} from ${payload.Server.title} ${locationText}`,
-      footer_icon: payload.Account.thumb
-    }]
-  }, () => {});
+function notifySlack(payload, action) {
+  slack.webhook(
+    {
+      channel,
+      username: "Plex",
+      icon_emoji: ":plex:",
+      attachments: [
+        {
+          fallback: "Required plain-text summary of the attachment.",
+          color: "#a67a2d",
+          title: formatTitle(payload.Metadata),
+          text: formatSubtitle(payload.Metadata),
+          footer: `${action} by ${payload.Account.title}`,
+          footer_icon: payload.Account.thumb,
+        },
+      ],
+    },
+    () => {}
+  );
 }
